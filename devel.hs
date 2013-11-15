f2ceaabc6dfa3e4fbf2a840d70c81b2cf2882782
BJTerry <bjterry@bjdesktop.local>
2013-11-14 19:53:03 -0800

    Initial commit

---
+++ devel.hs
@@ -0,0 +1,29 @@
+{-# LANGUAGE PackageImports #-}
+import "SocketBug" Application (getApplicationDev, webSocketHandler)
+import Network.Wai.Handler.Warp
+    (runSettings, defaultSettings, settingsPort, settingsIntercept)
+import Control.Concurrent (forkIO)
+import System.Directory (doesFileExist, removeFile)
+import System.Exit (exitSuccess)
+import Control.Concurrent (threadDelay)
+
+import Network.Wai.Handler.WebSockets (intercept)
+
+main :: IO ()
+main = do
+    putStrLn "Starting devel application"
+    (port, app) <- getApplicationDev
+    runSettings defaultSettings
+        { settingsPort = port
+        , settingsIntercept = intercept $ webSocketHandler
+        } app
+    loop
+
+loop :: IO ()
+loop = do
+  threadDelay 100000
+  e <- doesFileExist "yesod-devel/devel-terminate"
+  if e then terminateDevel else loop
+
+terminateDevel :: IO ()
+terminateDevel = exitSuccess
